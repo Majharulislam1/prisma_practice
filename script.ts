c4522846cafb722518d600c488d1fcdec4060bca
Majharulislam <majhurulislam535@gmail.com>
2026-02-05 16:39:58 +0600

    crud operation

--- script.ts
+++ script.ts
@@ -4,14 +4,50 @@ import { prisma } from "./lib/prisma";
 async function main() {
      
 
-    const user = await prisma.user.create({
-         data:{
-             name :"majharul islam",
-             email:"majharul@gmail.com"
+    // const user = await prisma.user.create({
+    //      data:{
+    //          name :"Ariful islam",
+    //          email:"majharul@gmail.com"
+    //      }
+    // })
+
+    const result = await prisma.user.findMany();
+     
+    // const update_user = await prisma.user.update({
+    //      where:{
+    //         id:1
+    //      },
+    //      data:{
+    //          name:"Mahadi hasan"
+    //      }
+    // })
+
+
+    // const delete_user = await prisma.user.delete({
+    //      where: {
+    //         id:2
+    //      }
+    // })
+
+    const orderBY = await prisma.user.findMany(
+        {
+             orderBy:{
+                id:'asc'
+             }
+        }
+    )
+
+    const searching = await prisma.user.findMany({
+         where:{
+             name:{
+                 contains:'ma',
+                 mode:'insensitive'
+             }
          }
     })
 
-    console.log(user);
+    console.log(searching)
+
 
 }
 
